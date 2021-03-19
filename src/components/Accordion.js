c739f711615aaa0f8a9c6e68219bd1f4c90f2714
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-03-19 11:34:55 +0200

    new commit

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,19 +1,47 @@
+import React, { useState } from 'react';
 import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
 import { Data } from './Data';
 import './Accordion.css';
 
 function Accordion() {
+    const [clicked, setClicked] = useState(false);
+
+    const toogle = (index) => {
+        if (clicked === index) {
+            return setClicked(null);
+        }
+
+        setClicked(index);
+    };
+
     return (
         <div className="Accordion">
             <div className="wrapper-accordion">
                 {Data.map((item, index) => {
                     return (
                         <div className="accordion-element">
-                            <h1 className="question">
-                                {item.question}{' '}
-                                <FaAngleDown className="arrow-btn"></FaAngleDown>
+                            <h1
+                                className={
+                                    'question ' +
+                                    (clicked === index ? 'question-active' : '')
+                                }
+                                onClick={() => toogle(index)}
+                                key={index}
+                            >
+                                {item.question}
+                                <span>
+                                    {clicked === index ? (
+                                        <FaAngleUp className="arrow-btn"></FaAngleUp>
+                                    ) : (
+                                        <FaAngleDown className="arrow-btn"></FaAngleDown>
+                                    )}
+                                </span>
                             </h1>
-                            <p className="answer">{item.answer}</p>
+                            {clicked === index ? (
+                                <p className={'answer'}>{item.answer}</p>
+                            ) : (
+                                ''
+                            )}
                         </div>
                     );
                 })}
